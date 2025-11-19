ff7d027193f3bf4c4960f631a67e57757dde7427
ardiansa22 <ferianardiansa22@gmail.com>
2025-11-19 09:27:58 +0700

    fix chatbot

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -16,7 +16,7 @@ class ChatbotController extends Controller
     public function send(Request $request)
     {
         // kirim pesan user ke webhook n8n
-        $response = Http::post('https://ferianfellow.app.n8n.cloud/webhook/fecc2cae-0230-41c0-8c4c-1ba3d6b5e253', [
+        $response = Http::post('https://dianadevi239.app.n8n.cloud/webhook/5b01e659-4fae-48d9-bd9b-bd5db50d3b22', [
             'message' => $request->input('message'),
             'session_id' => $request->input('session_id') ?? session()->getId(),
         ]);
